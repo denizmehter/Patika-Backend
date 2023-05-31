cc4fc439647aff1deeee3262485d0242be593c66
Deniz Mehter <denizmehter@gmail.com>
2023-05-31 21:52:39 +0300

    HW4 finished

--- Node/HW4.js
+++ Node/HW4.js
@@ -9,4 +9,14 @@ fs.writeFile("Node/employees.json", `{"name": "Employee 1 Name", "salary": 2000}
 fs.readFile('Node/employees.json', 'utf8', (err,data)=>{
     if (err) throw err;
     console.log(data);
-}); 
+});
+
+fs.appendFile('Node/employees.json', '\n{"name": "Employee 2 Name", "salary": 1000}', (err,data)=>{
+    if (err) throw err;
+    console.log('File has been updated')
+});
+
+fs.unlink("Node/employees.json", (err) => {
+    if (err) throw err;
+    console.log("The file has been removed.");
+});
